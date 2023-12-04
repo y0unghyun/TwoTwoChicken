d00f93748260cd1720100140833169c73435913d
BINDUNGLIFE <144803017+BINDUNGLIFE@users.noreply.github.com>
2023-12-04 21:17:52 +0900

    main.swift 업데이트_김가빈

--- TwoTwoChicken/TwoTwoChicken/main.swift
+++ TwoTwoChicken/TwoTwoChicken/main.swift
@@ -8,6 +8,18 @@
 import Foundation
 
 class Menu {
+    func printChickenMenu() {
+           print(" [치킨 메뉴] ")
+           print("1. 허니콤보")
+           print("2. 레드콤보")
+           print("3. 반반콤보")
+           print("4. 반반윙")
+           print("5. 레드윙")
+           print("6. 점보윙")
+           print("7. 허니점보윙")
+           print("0. 뒤로가기")
+       }
+    
     func printMenu() {
         // 메뉴 출력 부분
         print(" [TwoTwoChicken's MENU] ")
@@ -16,17 +28,22 @@ class Menu {
         print("3. Drinks        | 콜라, 사이다 등 다양한 음료 메뉴")
         print("4. Beers         | 치킨의 영원한 단짝, 시원한 맥주")
         print("0. 키오스크 종료    | 주문을 종료합니다.")
-        
-        guard var input = readLine() else {
-            print("error")
-            return
-        } // 옵셔널 바인딩
-        
-        var intInput: Int? = Int(input) //
-        
-        if let intInput = intInput {
-            // 입력으로 받은 번호에 따라서 세부 메뉴판 호출
+    
+        guard let input = readLine(), let number = Int(input) else {
+                    print("잘못된 입력입니다.")
+                    return
         }
+        
+        switch number {
+                case 1:
+                    printChickenMenu()
+                // 나머지 메뉴 구현
+                case 0:
+                    print("키오스크를 종료합니다.")
+                    return
+                default:
+                    print("잘못된 입력입니다. 다시 입력해주세요.")
+                }
         // 따로 초기화 구문은 작성하지 않았습니다.
         // 나중에 소지금액 및 결제 단계에 들어서 작성할 예정 입니다.
     }
