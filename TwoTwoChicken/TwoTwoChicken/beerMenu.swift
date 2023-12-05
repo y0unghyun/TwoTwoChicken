b4ecd77723eff0da359e61cac21999559e0fcdeb
RACHEL056 <parkmj0506@gmail.com>
2023-12-05 14:04:16 +0900

    add printBMenu func

--- TwoTwoChicken/TwoTwoChicken/beerMenu.swift
+++ TwoTwoChicken/TwoTwoChicken/beerMenu.swift
@@ -8,7 +8,13 @@
 import Foundation
 
 class beerMenu{
-    func printBMenu{
+    func printBMenu() {
         //맥주 메뉴 출력
+        print("1. Philsner       | 뒷맛이 쌉쌀하면서 깔끔한 가벼운 체코식 맥주")
+        print("2. Weiss          | 진한 과일향과 부드러운 목넘김이 매력적인 밀맥주")
+        print("3. Dunkles        | 달콤하고 고소한 향이 어우러져 진한 풍미가 일품인 흑맥주")
+        print("4. Weissbockbier  | 깊이 있고 부드러운 과일향을 가진 고알콜맥주")
+        print("0. 키오스크 종료    | 주문을 종료합니다.")
+        
     }
 }
