0036e4881d5e7766b61a48af2b6c4ab3eecc36bf
BINDUNGLIFE <144803017+BINDUNGLIFE@users.noreply.github.com>
2023-12-07 19:51:15 +0900

    Update ChickenMenu.swift

--- TwoTwoChicken/TwoTwoChicken/ChickenMenu.swift
+++ TwoTwoChicken/TwoTwoChicken/ChickenMenu.swift
@@ -7,35 +7,51 @@
 
 import Foundation
 
-class ChickenMenu : Menu {
-    override func printMenu() {
-        while true {
-            print(" [치킨 메뉴] ")
-            print("1. 허니콤보")
-            print("2. 레드콤보")
-            print("3. 반반콤보")
-            print("4. 반반윙")
-            print("5. 레드윙")
-            print("6. 점보윙")
-            print("7. 허니점보윙")
-            print("0. 뒤로가기")
+class ChickenMenu : MenuRef {
+    func printMenu() {
+        // 치킨 메뉴 출력
+        print(print("""
+              *-----------------------------------------------------------------------*
+              |                      [TwoTwoChicken's Chicken MENU]                   |
+              *-----------------------------------------------------------------------*
+              | 1. 허니콤보        |W 21.9| 뒷맛이 쌉쌀하면서 깔끔한 가벼운 체코식 맥주             |
+              | 2. 레드콤보        |W 21.9| 진한 과일향과 부드러운 목넘김이 매력적인 밀맥주          |
+              | 3. 반반콤보        |W 22.9| 달콤하고 고소한 향이 어우러져 진한 풍미가 일품인 흑맥주    |
+              | 4. 레드윙          |W 23.9| 깊이 있고 부드러운 과일향을 가진 고알콜맥주            |
+              | 0. 메뉴 돌아가기     | 메인 메뉴판으로 돌아갑니다                                |
+              *-----------------------------------------------------------------------*
+ """))
+      //      print(" [치킨 메뉴] ")
+      //      print("1. 허니콤보")
+      //      print("2. 레드콤보")
+      //      print("3. 반반콤보")
+      //      print("4. 레드윙")
+
             
-            guard let input = readLine(), let number = Int(input) else {
-                print("error: 숫자를 적어주세요.")
-                continue
-            }
-            
-            switch number {
-            case 1...7:
-                print("선택된 치킨 메뉴: \(number)")
-                return // 선택된 치킨 메뉴에 대한 추가 처리
-            case 0:
-                print("이전 메뉴판으로 돌아갑니다!")
+        if let intInput = Int(input) {
+            switch intInput {
+            case 1:
+                print("허니콤보를 골랐습니다.")
                 return
+            case 2:
+                print("레드콤보를 골랐습니다")
+                return
+            case 3:
+                print("반반콤보를 골랐습니다")
+                return
+            case 4:
+                print("레드윙를 골랐습니다")
+                return
+            case 0:
+                quitMenu()
             default:
                 print("error: 잘못된 입력입니다. 다시 한 번 세부 메뉴 번호를 입력해주세요.")
-                print(printMenu())
+                printMenu()
             }
         }
+    }
+    
+    func quitMenu() {
+        Menu().printMenu()
     }
 }
