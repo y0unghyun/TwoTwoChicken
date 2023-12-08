a0f485717bed3853ccdd346f31a505d7bd2ca340
y0unghyun <dl3rpika@gmail.com>
2023-12-08 10:56:07 +0900

    master branch created

--- TwoTwoChicken/TwoTwoChicken/sidemenu.swift
+++ TwoTwoChicken/TwoTwoChicken/sidemenu.swift
@@ -1,6 +1,6 @@
 //
 //  sidemenu.swift
-//  
+//
 //
 //  Created by t2023-m0032 on 12/8/23.
 //
@@ -8,44 +8,43 @@
 import Foundation
 
 class SideMenu: MenuRef {
-    
+    // printMenu 함수 시작
     func printMenu() {
         //매뉴 출력
         print("""
-
-                *------------------------------------------------------------*
-                |             [TwoTwoChicken's SIDE MENU]                    |
-                *------------------------------------------------------------*
-                | 1. 케이준 감자튀김   |W 2.5| 깊게 튀겨 바삭하며 고소한 감자튀김.        |
-                | 2. 버팔로윙        |W 3.5| 훈제 맛과 특별한 소스로 유명한 사이드.      |
-                | 3. 치즈볼          |W 2.5| 고소한 맛과 쫄깃함 속 달달한 크림치즈.     |
-                | 4. 콜팝           |W 3.5| 아이들이 밥안먹고 먹기만 하는 작은 친구들    |
-                | 0. Finish        | 주문을 종료합니다.                           |
-                *------------------------------------------------------------*
-
+                *-------------------------------------------------*
+                |             [TwoTwoChicken's SIDE MENU] |       |
+                *-------------------------------------------------*
+                | 1. 케이준 감자튀김   | 깊게 튀겨 바삭하며 고소한 감자튀김.    |
+                | 2. 버팔로윙        | 훈제 맛과 특별한 소스로 유명한 사이드.  |
+                | 3. 치즈볼         | 고소한 맛과 쫄깃함 속 달달한 크림치즈.  |
+                | 4. 콜팝          | 아이들이 밥안먹고 먹기만 하는 작은 친구들 |
+                | 0. Finish       | 주문을 종료합니다.                  |
+                *-------------------------------------------------*
 
 
 """)
     }
+    // printMenu 함수 끝
     
+    
+    // choiceMenu 함수 시작
     func choiceMenu() {
         
-        guard var input = readLine(), let input = Int(input) else {
+        guard let input = readLine(), let input = Int(input) else {
             print("error: 숫자를 적어주세요.")
             return
         } //옵셔널 바인딩
-        
         switch input {
-            
-        case 1 "케이준 감자튀김":
+        case 1 :
             print("케이준 감자튀김을 선택 하였습니다.")
-        case 2  "버팔로윙":
+        case 2 :
             print("버팔로윙을 선택 하였습니다.")
-        case 3  "치즈볼"
+        case 3 :
             print("치즈볼을 선택 하였습니다.")
-        case 4 "콜팝"
+        case 4 :
             print("콜팝을 선택 하였습니다.")
-        case 0
+        case 0 :
             quitMenu()
             // 메뉴 선택 옵션 case (1 ~ 5, 0 = quitmenu)
             
@@ -55,31 +54,11 @@ class SideMenu: MenuRef {
             // 오류 메세지에 대한 알림
         }
     }
-    
-    
-    
+    // choiceMenu 함수 끝
+    // quitMenu 함수 시작
     func quitMenu() {
-        Menu().printMenu()
+        MainMenu().printMenu()
     }
+    // quitMenu 함수 끝
+    
 }
-
-class fries : MenuDetailRef {
-    var price: Double = 2.5
-    var name: String = "케이준 감자튀김"
-}
-
-class wing : MenuDetailRef {
-    var price: Double = 3.5
-    var name: String = "버팔로윙"
-}
-
-class cheeseball : MenuDetailRef {
-    var price: Double = 2.5
-    var name: String = "치즈볼"
-}
-
-class colpop : MenuDetailRef {
-    var price: Double = 3.5
-    var name: String = "콜팝"
-}
-        
